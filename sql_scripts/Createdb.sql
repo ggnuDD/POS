b0db9505521af89d2daafc8025fabe963fb3a1c7
Toan Le <82274052+TimoWoodner@users.noreply.github.com>
2021-11-16 15:45:14 +0700

    update sql
    update create table
    insert data

--- sql_scripts/Createdb.sql
+++ sql_scripts/Createdb.sql
@@ -7,7 +7,7 @@ GO
 --GIA VE
 CREATE TABLE BANGGIA
 (	
-	thoi_gian		NVARCHAR(20) NOT NULL,
+	thoi_gian		NVARCHAR(50) NOT NULL,
 	gia_tre_em		INT NOT NULL,
 	gia_nguoi_lon	INT NOT NULL,
 	gia_HSSV		INT NOT NULL --hoc sinh/ sinh vien
@@ -22,7 +22,7 @@ CREATE TABLE DSPHIM
 	ten_phim		NVARCHAR(100) 	NOT NULL,
 	quoc_gia		NVARCHAR(50) 	NOT NULL,
 	the_loai		NVARCHAR(50) 	NOT NULL,
-	dao_dien		NVARCHAR(100) 	NOT NULL,
+	dao_dien		NVARCHAR(50) 	NOT NULL,
 	dien_vien		NVARCHAR(100) 	NOT NULL,
 	noi_dung		NVARCHAR(200) 	NOT NULL,
 	thoi_luong		TIME			NOT NULL, --hh:mm
@@ -68,50 +68,60 @@ CREATE TABLE LICHCHIEU
 );
 GO
 
---10. vé
-CREATE TABLE VE
-(
-	ma_ve			CHAR(15)		NOT NULL,		
-    --ma ve = ma rap + loai ve + ngay + suat chieu
-	ngay_gio_mua	SMALLDATETIME	NOT NULL,
-	loai_ve			INT				NOT NULL, 
-	-- cac loai ve: 01- nguoi lon, 02- tre em, 03- HSSV; 10- nguoi lon ngay le, 20- tre em ngay le, 30- tre em ngay le
-	ngay			DATE			NOT NULL,
-	ma_rap			VARCHAR(10)		NOT NULL,
-	suat_chieu		TIME			NOT NULL, --hh:mm
-	gia				INT				NOT NULL,
-	ma_ghe			CHAR(5)			NOT NULL,
-	ma_khach_hang	VARCHAR(255)	NULL, -- =username khach hang neu dat mua online, null neu mua truc tiep tai rap
-	PRIMARY KEY (ma_ve),
-	FOREIGN KEY (ngay, ma_rap, suat_chieu) REFERENCES dbo.LICHCHIEU(ngay, ma_rap, suat_chieu)
-);
-GO
-
 CREATE TABLE NHANVIEN
 (
 	ma_nhan_vien	CHAR(6)		NOT NULL
 		CHECK(ma_nhan_vien LIKE 'NV[0-9][0-9][0-9][0-9]'),
+	ho_ten			NVARCHAR(50) 	NOT NULL,
 	loai_cong_viec	VARCHAR(30) NOT NULL,
 	noi_lam_viec	VARCHAR(10)	NULL, -- null khong lam tai rap
 	ngay_sinh		VARCHAR(12) NOT NULL,
 	email			VARCHAR(30) NOT NULL,
-	gioi_tinh		CHAR(1)		NOT NULL,
-	dth_di_dong		INT			NOT NULL,
-	dth_noi_bo		INT			NOT NULL,
+	gioi_tinh		CHAR(5)		NOT NULL,
+	so_dien_thoai	INT			NOT NULL,
 	PRIMARY KEY(ma_nhan_vien),
 	FOREIGN KEY(noi_lam_viec) REFERENCES dbo.DSRAP(ma_rap),
 );
 GO
 
+--10. vé
+CREATE TABLE VE
+(		
+	ngay_gio_mua	SMALLDATETIME	NOT NULL,
+	loai_ve			INT				NOT NULL, 
+	-- cac loai ve: 01- nguoi lon, 02- tre em, 03- HSSV; 10- nguoi lon ngay le, 20- tre em ngay le, 30- tre em ngay le
+	ngay			DATE			NOT NULL, --ngay di xem phim
+	ma_rap			VARCHAR(10)		NOT NULL,
+	suat_chieu		TIME			NOT NULL, --hh:mm
+	gia				INT				NOT NULL,
+	ma_ghe			CHAR(5)			NOT NULL,
+	ma_khach_hang	VARCHAR(255)	NULL, -- =username khach hang neu dat mua online, null neu mua truc tiep tai rap
+	id_temp			INT IDENTITY(1,1),
+	ma_ve			AS 'V' + ma_rap 
+						   + right('00' + cast(loai_ve as varchar(2)), 2) 
+						   + right('00' + cast(DAY(GETDATE()) as varchar(2)), 2)
+						   + right('00' + cast(MONTH(GETDATE()) as varchar(2)), 2)
+						   + right('0000' + cast(YEAR(GETDATE()) as varchar(4)), 4)
+						   + right('0000' + cast(id_temp as varchar(4)), 4),
+	--ma ve = V + ma rap + loai ve + DDMMYYYY + [0-9][0-9][0-9][0-9]
+	FOREIGN KEY (ngay, ma_rap, suat_chieu) REFERENCES dbo.LICHCHIEU(ngay, ma_rap, suat_chieu)
+);
+GO
+
 CREATE TABLE DONHANG
 (
-	ma_bien_lai			CHAR(15)		NOT NULL PRIMARY KEY,
-    --ma bien lai: B + ma rap + ngay gio mua
 	ma_rap				VARCHAR(10),
 	ngay_gio_mua		SMALLDATETIME	NOT NULL,
 	tien_ve				INT DEFAULT 0,	
 	tien_do_an			INT DEFAULT 0,
 	tong				AS tien_ve + tien_do_an,
+	id_temp				INT IDENTITY(1,1),
+	ma_bien_lai			AS 'B' + ma_rap 
+						   + right('00' + cast(DAY(GETDATE()) as varchar(2)), 2)
+						   + right('00' + cast(MONTH(GETDATE()) as varchar(2)), 2)
+						   + right('0000' + cast(YEAR(GETDATE()) as varchar(4)), 4)
+						   + right('0000' + cast(id_temp as varchar(4)), 4),
+	--ma bien lai: B + ma rap + DDMMYYYY + [0-9][0-9][0-9][0-9]
 	FOREIGN KEY(ma_rap) REFERENCES dbo.DSRAP(ma_rap)
 );
 GO
---
+++ sql_scripts/InsertData.sql
@@ -0,0 +1,45 @@
+﻿USE CINEMA
+GO
+
+-- Them N khi go dau
+--insert bang gia
+INSERT INTO BANGGIA VALUES (N'Thứ 2-6', 40000, 50000, 45000);
+INSERT INTO BANGGIA VALUES (N'Cuối tuần - Ngày lễ', 50000, 60000, 55000);
+GO
+
+
+--insert phim
+INSERT INTO DSPHIM VALUES ('ff9','Fast and furious 9',N'Mỹ',N'Hành động','James Wan','Vin Diesel, John Cena',
+							N'Bom tấn đua xe trở lại và lợi hại hơn xưa','02:25:00','/images/poster/ff.png','youtube.com');
+							-- Mô tả phim có thể thay thế bằng đường dẫn tới file txt
+GO
+
+--insert danh sach rap
+INSERT INTO DSRAP VALUES ('HCM01',N'Vincom Thủ Đức',N'216 Võ Văn Ngân, Phường Bình Thọ, Quận Thủ Đức','TP HCM',3);
+INSERT INTO DSRAP VALUES ('HCM02','Pearl Plaza',N'561A Điện Biên Phủ, P.25, Quận Bình Thạnh','TP HCM',3);
+
+--phong chieu
+INSERT INTO PHONGCHIEU VALUES ('HCM01',1);
+INSERT INTO PHONGCHIEU VALUES ('HCM01',2);
+INSERT INTO PHONGCHIEU VALUES ('HCM01',3);
+INSERT INTO PHONGCHIEU VALUES ('HCM02',1);
+
+
+--lich chieu
+INSERT INTO LICHCHIEU VALUES ('2021-11-20','HCM01','08:00:00',1,'ff9');
+INSERT INTO LICHCHIEU VALUES ('2021-11-20','HCM01','08:15:00',2,'ff9');
+INSERT INTO LICHCHIEU VALUES ('2021-11-20','HCM01','09:00:00',3,'ff9');
+GO
+
+--nhan vien
+INSERT INTO NHANVIEN VALUES ('NV0001',N'Lê Khánh Toàn',N'Bán vé','HCM01','2001-11-15','toanle@gmail.com','Nam','0999009009');
+
+--ve
+INSERT INTO VE VALUES (GETDATE(),03,'2021-11-20','HCM01','08:00:00','45000','A9','toanlebk');
+INSERT INTO VE VALUES (GETDATE(),02,'2021-11-20','HCM01','08:00:00','50000','A2',NULL);
+
+--don hang
+INSERT INTO DONHANG VALUES ('HCM01', GETDATE(),'45000','20000');
+INSERT INTO DONHANG VALUES ('HCM01', GETDATE(),'50000','15000');
+
+SELECT * FROM DONHANG
