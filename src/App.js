9701a25b05979b7d0d690d3f29e865ac0cae4131
Toan Le <82274052+TimoWoodner@users.noreply.github.com>
2021-11-25 20:37:49 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import Home from "./pages/home";
-import Payment from "./pages/payment";
+import Payment from "./pages/Payment";
 import Movie from "./pages/movie";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
