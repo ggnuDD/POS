23b73e216c09eb49a9080b03fce4a2dff4d155b8
ggnuDD <93441263+ggnuDD@users.noreply.github.com>
2021-11-26 11:53:52 +0700

    temp admin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Home from "./pages/home";
 import Payment from "./pages/Payment";
 import Movie from "./pages/movie";
+import Admin from "./pages/admin";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SearchBar from "./components/SearchBar";
@@ -25,6 +26,7 @@ function App (){
             <Routes>
                 <Route path="/" exact element={<Home/>} />
                 <Route path="/home" exact element={<Home/>} />
+                <Route path="/admin" exact element={<Admin/>} />
                 <Route path="/payment" exact element={<Payment/>} />
                 <Route path="/movie" exact element={<Movie/>} />
                 <Route path="/login" exact element={<Login/>} />
